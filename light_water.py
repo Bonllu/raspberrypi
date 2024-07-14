a177184517a84ac226697ef24c7dd6e80922d917
Bonllu <151839401+Bonllu@users.noreply.github.com>
2024-07-14 20:30:09 +0200

    Update light_water.py

--- light_water.py
+++ light_water.py
@@ -9,15 +9,15 @@ def setup():
     GPIO.output(ledPins, GPIO.HIHG)
 
 def loop():
-  while True:
-    for pin in ledPins:
-        GPIO.output(pin, GPIO.LOW)
-        time.sleep(0.1)
-        GPIO.output(pin, GPIO.HIGH)
-    for pin in ledPins[::-1]
-        GPIO output(pin, GPIO.LOW)
-        time.sleep(0.1)
-        GPIO.output(pin, GPIO.HIGH) 
+    while True:
+        for pin in ledPins:
+            GPIO.output(pin, GPIO.LOW)
+            time.sleep(0.1)
+            GPIO.output(pin, GPIO.HIGH)
+        for pin in ledPins[::-1]
+            GPIO output(pin, GPIO.LOW)
+            time.sleep(0.1)
+            GPIO.output(pin, GPIO.HIGH) 
 
 def destroy():
     GPIO.cleanup()
